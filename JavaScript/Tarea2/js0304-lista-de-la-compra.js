432e2ecc5877bd2c32a284f9fad90deef9c4d3e9
Hugo Moruno <hmorunop01@educarex.es>
2024-09-20 00:46:38 +0200

    Terminada la Tarea 2

--- JavaScript/Tarea2/js0304-lista-de-la-compra.js
+++ JavaScript/Tarea2/js0304-lista-de-la-compra.js
@@ -2,13 +2,13 @@
 const prompt = require('prompt-sync')();
         
 var vacio = false;
-var listaCompra = [];
+var listaCompra = [];                                   //Se crea la lista de la compra (Array)
 
 do
 {
     console.log('Introduce un objeto a la lista (Vacio para terminar): ')
     var objetoIntroducido = prompt();
-    if (objetoIntroducido.trim().valueOf() != "")
+    if (objetoIntroducido.trim().valueOf() != "")       //Se comprueba que el objeto de la lista de la compra no está vacío
     {
         listaCompra.push(objetoIntroducido);
     } 
@@ -21,10 +21,11 @@ do
 
 console.log("Ordenando la lista...");
 
-listaCompra.sort();
+listaCompra.sort();                                     //Se ordena la lista según orden de caracteres de la Unicode.
 
 console.log('Ordenada!');
 
-listaCompra.forEach((objeto) => {
+listaCompra.forEach((objeto) =>                         //Se muestra la lista ordenada
+{
     console.log(objeto)
 });
