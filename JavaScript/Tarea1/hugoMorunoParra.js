2e8e0b765e5cc520bcb0c453c88b5f36d7279911
Hugo Moruno <hmorunop01@educarex.es>
2024-09-19 19:22:02 +0200

    Subida de Tarea01

--- JavaScript/Tarea1/hugoMorunoParra.js
+++ JavaScript/Tarea1/hugoMorunoParra.js
@@ -5,19 +5,22 @@ const prompt = require('prompt-sync')();
 let numeroDeIntentos = 5;
 let numeroencontrado = false;
 
-console.log("No se lo digas a nadie (chsssss): " + NUMEROSECRETO);
+// console.log("No se lo digas a nadie (chsssss): " + NUMEROSECRETO);  //Te chiva el número
 
 do 
 {
-    console.log(numeroDeIntentos);
-    console.log(numeroencontrado);
-    entradaDeNumero = prompt('¿Qué número crees que es el ganador? (Entre el 0 y el 1000): ');
+    // console.log(numeroDeIntentos);       // Te lee el número de intentos
+    // console.log(numeroencontrado);       // Te lee el estado del booleano de salida
+    entradaDeNumero = prompt('¿Qué número crees que es el ganador? (Entre el 0 y el 1000) (Intentos restantes: ', numeroencontrado,' ): ');      //Lee por consola el número
     if (entradaDeNumero == 'exit')      //Cierra la aplicación
+    {
         window.close();
+    }
     if (parseInt(entradaDeNumero))      //Comprueba si el texto introducido es un número
-        { 
+    { 
         if (entradaDeNumero >= 0 && entradaDeNumero <= 1000)  //Comprueba que el número está en el arco especificado
-            if (entradaDeNumero == NUMEROSECRETO)             //
+        {
+            if (entradaDeNumero == NUMEROSECRETO)             //Comprueba que el número es el correcto y si lo es, cierra el bucle.
             {
                 console.log("ENHORABUENA!")
                 numeroencontrado = true;
@@ -27,7 +30,8 @@ do
             {
                 console.log("Número incorrecto")
                 numeroDeIntentos = numeroDeIntentos - 1;
-            }
+}                                                             //Si el número no es el correcto, repite el bucle restanto un intento.
+        }
         else
             console.log("Numero fuera del rango.")
     } 
@@ -36,6 +40,6 @@ do
         console.log("No es un número.")
     }
 } 
-while (!numeroencontrado || numeroDeIntentos > 0)
+while (!numeroencontrado || numeroDeIntentos > 0)               //Condición del bucle
     
 console.log("SE ACABÓ!")
