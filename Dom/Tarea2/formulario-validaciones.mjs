5f48f7ef8ac1940d044e2196aefbb22e8ca5285b
Hugo Moruno <dawhmorunop@iescastelar.com>
2024-10-10 19:20:57 +0200

    a

--- Dom/Tarea2/formulario-validaciones.mjs
+++ Dom/Tarea2/formulario-validaciones.mjs
@@ -1,3 +1,20 @@
+"use strict";
+
+/**
+ * @author = Hugo Moruno Parra
+ * 
+ * @fecha = '09/10/2024'
+ */
+
+export { 
+    limpiarFormulario,
+    infoCampos
+};
+
+/**
+ * @name = Variables de inicio.
+ */
+
 const OBJ_NOMBRE = document.getElementById('Nombre');
 const OBJ_APELLIDOS = document.getElementById('Apellidos');
 const OBJ_DNI = document.getElementById('Dni');
@@ -5,7 +22,11 @@ const OBJ_EMAIL = document.getElementById('Email');
 const OBJ_PASSWD = document.getElementById('Passwd');
 const OBJ_PASSWDREP = document.getElementById('Passwdrep');
 
-export function limpiarFormulario()
+/**
+ * @name = Funciones de validación.
+ */
+
+function limpiarFormulario()
 {
     OBJ_NOMBRE.value = "";
     OBJ_APELLIDOS.value = "";
@@ -13,4 +34,12 @@ export function limpiarFormulario()
     OBJ_EMAIL.value = "";
     OBJ_PASSWD.value = "";
     OBJ_PASSWDREP.value = "";
+}
+
+function infoCampos(campo, mostrar)
+{
+    if (mostrar)
+    {
+        console.log(campo.parentNode.target);
+    }
 }
--- Dom/Tarea2/formulario.mjs
+++ Dom/Tarea2/formulario.mjs
@@ -12,6 +12,8 @@ import * as validaciones from "./formulario-validaciones.mjs";
  * @name = Variables de inicio.
  */
 const BTN_LIMPIAR = document.getElementById('limpiar');
+const FORM = document.getElementById('formulario');
+const OBJ_CAMPOSFORM = document.getElementsByTagName('input');
 
 /**
  * @name = Main.
@@ -21,4 +23,29 @@ window.addEventListener('load', () => {
     
     BTN_LIMPIAR.addEventListener('click', validaciones.limpiarFormulario);
 
+    FORM.addEventListener('submit', (form) => {
+
+        if (validaciones.validarFormulario > 0)
+        {
+            validaciones.aviso;
+            validaciones.marcarCampos;
+            form.preventDefault();
+        }
+        else
+        {
+            validaciones.enviadoCorrectamente;
+        }
+    });
+
+    Array.from(OBJ_CAMPOSFORM).forEach((campoForm) => {
+        campoForm.addEventListener('focus', (campo) => {
+            validaciones.infoCampos(campo, true);
+        })     
+
+        campoForm.addEventListener('blur', (campo) => {
+            validaciones.infoCampos(campo, false);
+        })
+    });
+    
 });
+
