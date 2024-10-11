bf4e4855f864225e6bdc40d746c5761a84c5e49d
Hugo Moruno <dawhmorunop@iescastelar.com>
2024-10-12 01:49:17 +0200

    12-Octubre
    Tarea 2 - Formulario, terminada.

--- Dom/Tarea2/formulario.mjs
+++ Dom/Tarea2/formulario.mjs
@@ -11,45 +11,90 @@ import * as validaciones from "./formulario-validaciones.mjs";
 /**
  * @name = Variables de inicio.
  */
+
+/**
+ * Variables de objetos del documento.
+ */
 const BTN_LIMPIAR = document.getElementById('limpiar');
 const FORM = document.getElementById('formulario');
 const OBJ_CAMPOSFORM = document.getElementsByTagName('input');
+const OBJ_BTNRESUL = document.getElementById('submitResultButton');
 
 /**
  * @name = Main.
  */
 
+/**
+ * Evento que carga al terminar de cargarse el documento.
+ */
 window.addEventListener('load', () => {
     
+    /**
+     * Llamada al metodo de limpieza.
+     */
+
     BTN_LIMPIAR.addEventListener('click', validaciones.limpiarFormulario);
 
-    
+    /**
+     * Gestor de eventos del formulario.
+     * Focus: Enseña el campo de información pertinente.
+     * Blur: Borra el campo de información pertinente.
+     * Input: Comprueba en tiempo real que los campos sean correctos.
+     */
 
     Array.from(OBJ_CAMPOSFORM).forEach((campoForm) => {
         campoForm.addEventListener('focus', (campo) => {
-            validaciones.infoCampos(campo, true);
+            validaciones.infoCampos(campo.target, true);
         });    
 
         campoForm.addEventListener('blur', (campo) => {
-            validaciones.infoCampos(campo, false);
+            validaciones.infoCampos(campo.target, false);
         });
 
         campoForm.addEventListener('input', (campo) => {
-            validaciones.validarCampo(campo);
+            validaciones.validarCampo(campo.target);
         });
     });
     
+    /**
+     * Gestor del evento submit.
+     */
+
     FORM.addEventListener('submit', (form) => {
 
-        if (validaciones.errores > 0)
+        //Si hay errores no envía el formulario y muestra la pantalla de errores.
+        if (validaciones.errores.length > 0)
         {
             form.preventDefault();
-            validaciones.errorAlEnviar;
+            validaciones.errorAlEnviar();
         }
         else
         {
-            validaciones.enviadoCorrectamente;
+            //Si no hay errores, pero algún campo no cumple los requisitos 
+            //(Funciones como autocompletar de los navegadores)
+            //valida todos los campos y según si hay errores o no actúa.
+            Array.from(OBJ_CAMPOSFORM).forEach((campoForm) => {
+                validaciones.validarCampo(campoForm);
+            });
+            if (validaciones.errores.length > 0)
+            {
+                form.preventDefault();
+                validaciones.errorAlEnviar();
+            }
+            else
+            {
+                validaciones.enviadoCorrectamente();
+                validaciones.limpiarFormulario();
+            }
+            
         }
+    });
+
+    /**
+     * Gestor del evento para ocultar la pantalla de información post-submit.
+     */
+    OBJ_BTNRESUL.addEventListener('click', () => {
+        validaciones.ocultarResultado()
     });
 
 });
