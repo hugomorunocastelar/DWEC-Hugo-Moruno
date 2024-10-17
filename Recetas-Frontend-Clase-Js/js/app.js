d0184691da31bb9ed1b468a64246a27342d76620
Hugo Moruno <dawhmorunop@iescastelar.com>
2024-10-17 21:25:58 +0200

    17 - Octubre

---
+++ Recetas-Frontend-Clase-Js/js/app.js
@@ -0,0 +1,14 @@
+
+$(window).on('load', function(){
+
+    $('#workspace').load('./html/recetas.html');
+    function recetas()
+    {
+        $('#workspace').load('./html/recetas.html');
+    }
+    function ingredientes()
+    {
+        $('#workspace').load('./html/ingredientes.html');
+    }
+});
+
